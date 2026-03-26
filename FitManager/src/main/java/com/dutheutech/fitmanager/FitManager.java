b9eacd530d75044ff9b17a319ea17f1682b64254
PLAYER3375 <matheus_gomes@ufms.br>
2026-03-26 00:03:50 -0300

    initial commit

---
+++ FitManager/src/main/java/com/dutheutech/fitmanager/FitManager.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.dutheutech.fitmanager;
+
+/**
+ *
+ * @author Kabum
+ */
+public class FitManager {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
