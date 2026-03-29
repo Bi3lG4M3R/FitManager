1ea5d220d35e488bdf586a636a7f0277b1c93240
PLAYER3375 <matheus_gomes@ufms.br>
2026-03-29 15:20:23 -0300

    First push with enum PlanType

---
+++ FitManager/src/main/java/domain/plan/PlanType.java
@@ -0,0 +1,24 @@
+package domain.plan;
+
+public enum PlanType {
+    MONTHLY(1, "Mensal"),
+    QUARTERLY(2, "Trimestral"),
+    SEMI_ANNUAL(3, "Semestral"),
+    ANNUAL(4, "Anual");
+    
+    int option;
+    String description;
+    
+    PlanType(int option, String description){
+        this.option = option;
+        this.description = description;
+    }
+    
+    public int getValorOpcao(){
+        return option;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
