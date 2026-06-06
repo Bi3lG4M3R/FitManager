84f94a704f69a06070d409506def6bda5712af81
Gabriel Richard Zambianchi de Oliveira <143223160+Bi3lG4M3R@users.noreply.github.com>
2026-06-06 03:50:24 -0400

    Criação do financialReport

---
+++ FitManager/src/application/FinancialReport.java
@@ -0,0 +1,31 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class FinancialReport {
+ 
+    private int month;
+    private int year;
+    private double totalRevenue;
+    private Map<String, Double> revenueByPlanType;
+    private Map<String, Double> revenueByPaymentType;
+    private double totalProcessingFees;
+    private int enrollmentsStarted;
+    private int enrollmentsCancelled;
+    private ArrayList<String> mostContractedPlanTypes;
+
+    public FinancialReport(int month, int year) {
+        this.month = month;
+        this.year = year;
+        this.totalRevenue = 0.0;
+        this.revenueByPlanType = new HashMap<>();
+        this.revenueByPaymentType = new HashMap<>();
+        this.totalProcessingFees = 0.0;
+        this.enrollmentsStarted = 0;
+        this.enrollmentsCancelled = 0;
+        this.mostContractedPlanTypes = new ArrayList<>();
+    }
+}
